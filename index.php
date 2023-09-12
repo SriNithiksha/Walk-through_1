304ad7a9a0cbfccdf2b63dc569e9a66d117deb2c
SriNithiksha <122574570+SriNithiksha@users.noreply.github.com>
2023-09-11 22:13:29 -0800

    update php file
    creating array to store collection of things using ordered and unordered list

--- index.php
+++ index.php
@@ -19,10 +19,11 @@
 
   ?>
   <ul>
-    <?php foreach ($books as $book) {
-      echo "<li>$book</li>";
-    }
-    ?>
+    <?php foreach ($books as $book) : ?> 
+    <li><?= $book ?></li>
+    <?php endforeach; ?>
+    
+  
   </ul>
 </body>
 

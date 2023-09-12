b87f222baef497fa5b73555845d6ddb078b8d17c
SriNithiksha <122574570+SriNithiksha@users.noreply.github.com>
2023-09-11 21:25:55 -0800

    add css style to the content

--- index.php
+++ index.php
@@ -1,13 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <title>Document</title>
-  </head>
-  <body>
-  <?php
-    $greeting = "hello";
-     
-    echo '$greeting everybody';
-    ?>
-  </body>
+
+<head>
+  <title>Document</title>
+  <style>
+   body {
+      display: grid;
+      place-items: center;
+      height: 100%;
+      margin: 0;
+      font-family: sans-serif;
+    }
+    
+  </style>
+</head>
+
+<body>
+  <h1>
+    You have read "PHP & MYSQL"
+  </h1>
+</body>
+
 </html>

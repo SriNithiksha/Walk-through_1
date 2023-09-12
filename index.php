0f03b67b67c320138cee99a2cb62b21f397126d3
SriNithiksha <122574570+SriNithiksha@users.noreply.github.com>
2023-09-12 08:16:32 -0800

    creating separate files for php & html

--- index.php
+++ index.php
@@ -1,53 +1,29 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
 
-<head>
-  <title>Document</title>
+$books = [
+  [
+    'name' => 'Do androids dream of electric sheep',
+    'author' => 'Philip K. Dick',
+    'releaseYear' => 1968,
+    'purchaseUrl' => 'http: //example.com'
+  ],
+  [
+    'name' => 'Project hail mary',
+    'author' => 'Andy Weir',
+    'releaseYear' => 2021,
+    'purchaseUrl' => 'http: //example.com'
+  ],
+  [
+    'name' => 'The Martian',
+    'author' => 'Andy Weir',
+    'releaseYear' => 2011,
+    'purchaseUrl' => 'http: //example.com'
+  ],
+];
 
-</head>
+$filteredBooks = array_filter($books, function ($book) {
+  return $book['author'] === 'Andy Weir';
+});
 
-<body>
-  <h1>Recommended Books</h1>
+?>
 
-  <?php
-
-  $books = [
-    [
-      'name' => 'Do androids dream of electric sheep',
-      'author' => 'Philip K. Dick',
-      'releaseYear' => 1968,
-      'purchaseUrl' => 'http: //example.com'
-    ],
-    [
-      'name' => 'Project hail mary',
-      'author' => 'Andy Weir',
-      'releaseYear' => 2021,
-      'purchaseUrl' => 'http: //example.com'
-    ],
-    [
-      'name' => 'The Martian',
-      'author' => 'Andy Weir',
-      'releaseYear' => 2011,
-      'purchaseUrl' => 'http: //example.com'
-    ],
-  ];
-
-  $filteredBooks = array_filter($books, function ($book) {
-    return $book ['author'] === 'Andy Weir';
-  });
-
-  ?>
-  <ul>
-    <?php foreach ($filteredBooks as $book): ?>
-      <li>
-        <a href="<?= $book['purchaseUrl'] ?>">
-          <?= $book['name']; ?> (<?= $book['releaseYear'] ?>) - By <?= $book['author'] ?>
-        </a>
-      </li>
-
-    <?php endforeach; ?>
-  </ul>
-
-</body>
-
-</html>

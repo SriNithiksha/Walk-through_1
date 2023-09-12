c7767e2c4c9100d3632fba3ba6bce2e94790cd22
SriNithiksha <122574570+SriNithiksha@users.noreply.github.com>
2023-09-12 07:01:00 -0800

    update php code using array function

--- index.php
+++ index.php
@@ -15,41 +15,33 @@
     [
       'name' => 'Do androids dream of electric sheep',
       'author' => 'Philip K. Dick',
-      'releaseyear' => 1968,
+      'releaseYear' => 1968,
       'purchaseUrl' => 'http: //example.com'
     ],
     [
       'name' => 'Project hail mary',
       'author' => 'Andy Weir',
-      'releaseyear' => 2021,
+      'releaseYear' => 2021,
       'purchaseUrl' => 'http: //example.com'
     ],
     [
       'name' => 'The Martian',
       'author' => 'Andy Weir',
-      'releaseyear' => 2011,
+      'releaseYear' => 2011,
       'purchaseUrl' => 'http: //example.com'
     ],
   ];
 
-  function filterByAuthor($books)
-  {
-    $filteredBooks = [];
+  $filteredBooks = array_filter($books, function ($book) {
+    return $book ['author'] === 'Andy Weir';
+  });
 
-    foreach ($books as $book) {
-      if ($book['author'] === 'Andy Weir') {
-        $filteredBooks[] = $book;
-      }
-    }
-
-    return $filteredBooks;
-  }
   ?>
   <ul>
-    <?php foreach (filterByAuthor($books, 'Philip K. Dick') as $book): ?>
+    <?php foreach ($filteredBooks as $book): ?>
       <li>
         <a href="<?= $book['purchaseUrl'] ?>">
-          <?= $book['name']; ?> (<?= $book['releaseyear'] ?>) - By <?= $book['author'] ?>
+          <?= $book['name']; ?> (<?= $book['releaseYear'] ?>) - By <?= $book['author'] ?>
         </a>
       </li>
 
